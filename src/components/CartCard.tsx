4a82089a112c405fd8df6abdc7602abea7dea921
samuelj1323 <samuelj1323@gmail.com>
2025-01-20 17:28:51 -0500

    Updating the card functionality to be adding subtracting and deleting.

--- src/components/CartCard.tsx
+++ src/components/CartCard.tsx
@@ -7,7 +7,28 @@ import {
   Button,
   CardActions,
 } from "@mui/material";
-const CartCard = (item: CartItem) => {
+import RemoveIcon from "@mui/icons-material/Remove";
+import AddIcon from "@mui/icons-material/Add";
+import DeleteIcon from "@mui/icons-material/Delete";
+export interface CartItem {
+  name: string;
+  price: number;
+  image: string;
+  amount: number;
+  id: number;
+}
+interface CartCardProps {
+  item: CartItem;
+  handleRemoveFromCart: (item: CartItem) => void;
+  handleAddToCart: (item: CartItem) => void;
+  handleSubtractFromCart: (item: CartItem) => void;
+}
+const CartCard = ({
+  item,
+  handleRemoveFromCart,
+  handleAddToCart,
+  handleSubtractFromCart,
+}: CartCardProps) => {
   return (
     <Grid size={4}>
       <Card>
@@ -16,21 +37,39 @@ const CartCard = (item: CartItem) => {
         >
           <img
             style={{ maxWidth: 200, width: "100%" }}
-            src={item.image}
-            alt={item.name}
+            src={item?.image}
+            alt={item?.name}
           />
         </CardMedia>
         <CardContent>
-          <Typography variant="h5">{item.name}</Typography>
-          <Typography variant="body1">{"$" + item.price}</Typography>
-          <Typography variant="body1">{item.amount}</Typography>
+          <Typography variant="h5">{item?.name}</Typography>
+          <Typography variant="body1">{"$" + item?.price}</Typography>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Button
+              style={{ textTransform: "none" }}
+              onClick={() => handleSubtractFromCart(item)}
+            >
+              <RemoveIcon />
+            </Button>
+            <Typography variant="body1">{item.amount}</Typography>
+            <Button
+              style={{ textTransform: "none" }}
+              onClick={() => handleAddToCart(item)}
+            >
+              <AddIcon />
+            </Button>
+            <Button onClick={() => handleRemoveFromCart(item)}>
+              <DeleteIcon />
+            </Button>
+          </div>
         </CardContent>
-        <CardActions>
-          <Button style={{ textTransform: "none" }}>
-            + <Typography>Add More</Typography>
-          </Button>
-          <Button style={{ textTransform: "none" }}>Remove</Button>
-        </CardActions>
+        <CardActions></CardActions>
       </Card>
     </Grid>
   );
--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,20 +1,16 @@
+import { useState, useCallback } from "react";
 import { Typography, Grid2 as Grid } from "@mui/material";
-import CartCard from "../components/CartCard";
-interface CartItem {
-  name: string;
-  price: number;
-  image: string;
-  amount: number;
-}
+import CartCard, { CartItem } from "../components/CartCard";
 
 const Cart = () => {
-  const cartItems: CartItem[] = [
+  const initialCartItems: CartItem[] = [
     {
       name: "Beef New york strip steak",
       price: 13.99,
       image:
         "https://www.traderjoes.com/content/dam/trjo/products/m20801/56224.png/jcr:content/renditions/webp-1280.webp",
       amount: 1,
+      id: 1,
     },
     {
       name: "Beef New york strip steak",
@@ -22,6 +18,7 @@ const Cart = () => {
       image:
         "https://www.traderjoes.com/content/dam/trjo/products/m20801/56224.png/jcr:content/renditions/webp-1280.webp",
       amount: 1,
+      id: 2,
     },
     {
       name: "Beef New york strip steak",
@@ -29,6 +26,7 @@ const Cart = () => {
       image:
         "https://www.traderjoes.com/content/dam/trjo/products/m20801/56224.png/jcr:content/renditions/webp-1280.webp",
       amount: 1,
+      id: 3,
     },
     {
       name: "Beef New york strip steak",
@@ -36,6 +34,7 @@ const Cart = () => {
       image:
         "https://www.traderjoes.com/content/dam/trjo/products/m20801/56224.png/jcr:content/renditions/webp-1280.webp",
       amount: 1,
+      id: 4,
     },
     {
       name: "Beef New york strip steak",
@@ -43,21 +42,56 @@ const Cart = () => {
       image:
         "https://www.traderjoes.com/content/dam/trjo/products/m20801/56224.png/jcr:content/renditions/webp-1280.webp",
       amount: 1,
+      id: 5,
     },
   ];
+  const [cartItems, setCartItems] = useState(initialCartItems);
   // This page should be like a list of all the items in your cart. Vertically stacked, an image present, and like the price that gets computed at the end.
 
-  const totalPrice = cartItems.reduce((acc, item) => acc + item.price, 0);
+  const totalPrice = () => {
+    return cartItems.reduce((acc, item) => acc + item.price * item.amount, 0);
+  };
+
+  const handleRemove = (item: CartItem) => {
+    const newList = cartItems.filter((i) => {
+      return i.id !== item.id;
+    });
+    setCartItems(newList);
+  };
+  const handleSubtract = (item: CartItem) => {
+    const newList = cartItems.map((i) => {
+      if (i.id === item.id) {
+        return { ...i, amount: i.amount - 1 };
+      }
+      return i;
+    });
+    setCartItems(newList);
+  };
+  const handleAdd = (item: CartItem) => {
+    const newList = cartItems.map((i) => {
+      if (i.id === item.id) {
+        return { ...i, amount: i.amount + 1 };
+      }
+      return i;
+    });
+    setCartItems(newList);
+  };
 
   return (
     <div>
       <Typography variant="h3">My cart</Typography>
       <Grid container spacing={2}>
         {cartItems.map((item) => (
-          <CartCard {...item} />
+          <CartCard
+            key={item.id}
+            item={item}
+            handleAddToCart={handleAdd}
+            handleRemoveFromCart={handleRemove}
+            handleSubtractFromCart={handleSubtract}
+          />
         ))}
       </Grid>
-      <Typography variant="h6">Total: ${totalPrice}</Typography>
+      <Typography variant="h6">Total: ${totalPrice()}</Typography>
     </div>
   );
 };
